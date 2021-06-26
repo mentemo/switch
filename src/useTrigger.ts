e8b5063c3983b2a80436ef185b6c7c339fb7b735
Qrymy <regist@qry.mx>
2021-06-27 00:57:41 +0900

    ini body param

--- src/useTrigger.ts
+++ src/useTrigger.ts
@@ -4,11 +4,12 @@ import { api } from './api'
 export const useTrigger = (url: string) => {
   const [loading, setLoading] = useState(false)
 
-  const trigger = async () => {
+  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+  const trigger = async (body?: Record<string, any>) => {
     setLoading(true)
 
     try {
-      const result = await api.post(url)
+      const result = await api.post(url, body)
 
       return result
     } catch (e) {
