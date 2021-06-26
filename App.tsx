cbd6d53bf0df1947f8a84ec7e7bc47c0c352a5f5
Qrymy <regist@qry.mx>
2021-06-26 23:00:15 +0900

    fix linting

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import { StatusBar } from 'expo-status-bar'
-import React from 'react'
+import React, { VFC } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
-export default function App() {
+const App: VFC = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -19,3 +19,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 })
+
+export default App
